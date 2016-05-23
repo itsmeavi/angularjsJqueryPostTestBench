c16edcd179a5700fe78df8a557d42b5a69bbd887
onclave <debabrata.acharya1993@gmail.com>
2016-05-23 18:59:29 +0530

    Image Upload

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -5,6 +5,26 @@ angular.module('postTestbench')
 	.controller('MainFormController', ['$scope', function($scope) {
 		$scope.msg = "submit nigga";
 
+		$scope.imageUpload = function (files) {
+			var data = new FormData();
+			data.append('file', files[0]);
+
+			$.ajax({
+				data: data,
+				type: 'POST',
+				url: "URL",
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function(url) {
+                    console.log(url);
+                },
+                error: function () {
+                	console.log("ERROR");
+                }
+			});
+		};
+
 		$scope.sendFormData = function () {
 			$.post('/test/deploy/post/form', $('#sampleForm').serialize())
 
